6b6e217341b55432b61511b44a895ae8550e4bfe
moses <agbaojemoses@gmail.com>
2026-02-16 17:29:14 +0100

    latest o

--- admin-app/server/index.js
+++ admin-app/server/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(router);
+app.use('/api', router);
 
  
 if (process.env.NODE_ENV === 'production') {
--- admin-app/server/routes/router.js
+++ admin-app/server/routes/router.js
@@ -16,65 +16,65 @@ router.use(session(sessionConfig));
 
 //GET REQUESTS 
 
-router.get("/api/confirm-email/:id", API.emailVerification);
+router.get("/confirm-email/:id", API.emailVerification);
 
-router.get("/api/fetch_admin", check_admin_session,  API.fetch_admin);
+router.get("/fetch_admin", check_admin_session,  API.fetch_admin);
 
-router.get("/api/fetch_products", check_admin_session, API.fetch_products);
+router.get("/fetch_products", check_admin_session, API.fetch_products);
 
-router.get("/api/fetch_customers", check_admin_session, API.fetch_customers);
+router.get("/fetch_customers", check_admin_session, API.fetch_customers);
 
-router.get("/api/fetch_categories", check_admin_session, API.fetch_categories);
+router.get("/fetch_categories", check_admin_session, API.fetch_categories);
 
-router.get("/api/fetch_settings", check_admin_session,  API.fetch_settings);
+router.get("/fetch_settings", check_admin_session,  API.fetch_settings);
 
-router.get("/api/fetch_orders", check_admin_session,  API.fetch_orders);
+router.get("/fetch_orders", check_admin_session,  API.fetch_orders);
 
-router.get("/api/logout", API.logout);
+router.get("/logout", API.logout);
 
  
 // POST REQUEST
 
-router.post("/api/register", API.register)
+router.post("/register", API.register)
 
-router.post("/api/resend-email-confirmation", API.ResendConfirmationMail)
+router.post("/resend-email-confirmation", API.ResendConfirmationMail)
 
-router.post("/api/login", API.login)
+router.post("/login", API.login)
 
-router.post("/api/cancel_order", check_admin_session, API.cancel_order)
+router.post("/cancel_order", check_admin_session, API.cancel_order)
 
-router.post("/api/confirm_order", check_admin_session, API.confirm_order)
+router.post("/confirm_order", check_admin_session, API.confirm_order)
 
-router.post("/api/retry_refund", check_admin_session, API.retry_refund)
+router.post("/retry_refund", check_admin_session, API.retry_refund)
 
-router.post("/api/upload_items", check_admin_session, HELPERS.handleUpload, API.upload_items);
+router.post("/upload_items", check_admin_session, HELPERS.handleUpload, API.upload_items);
   
-router.post("/api/update_photo", check_admin_session, HELPERS.handleUpload, API.update_photo);
+router.post("/update_photo", check_admin_session, HELPERS.handleUpload, API.update_photo);
 
-router.post("/api/update_video", check_admin_session, HELPERS.handleUpload, API.update_video);
+router.post("/update_video", check_admin_session, HELPERS.handleUpload, API.update_video);
 
-router.post("/api/send_reset_pass_email", API.send_reset_pass_email);
+router.post("/send_reset_pass_email", API.send_reset_pass_email);
 
-router.post("/api/reset_password", API.reset_password);
+router.post("/reset_password", API.reset_password);
  
 
 //PATCH REQUEST
 
-router.patch("/api/update_order_status", check_admin_session, API.update_order_status)
+router.patch("/update_order_status", check_admin_session, API.update_order_status)
 
-router.patch("/api/update_payment_status", check_admin_session, API.update_payment_status)
+router.patch("/update_payment_status", check_admin_session, API.update_payment_status)
 
-router.patch("/api/update_item", check_admin_session, API.update_item)
+router.patch("/update_item", check_admin_session, API.update_item)
 
-router.patch("/api/update_admin_info", check_admin_session, API.update_admin_info)
+router.patch("/update_admin_info", check_admin_session, API.update_admin_info)
 
-router.patch("/api/update_admin_pass", check_admin_session, API.update_admin_pass)
+router.patch("/update_admin_pass", check_admin_session, API.update_admin_pass)
 
 
 
 
 //DELETE REQUEST
 
-router.delete("/api/delete_video", check_admin_session, API.delete_video)
+router.delete("/delete_video", check_admin_session, API.delete_video)
 
 module.exports = router
